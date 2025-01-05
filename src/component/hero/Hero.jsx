b1485e111b76f4a3d9c4c31eac50109ebbfb7d4c
MosfiqurRubel <rubel.airin@gmail.com>
2025-01-06 01:14:01 +0600

    Hero and footer

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,7 +1,21 @@
+import heroImage from "../../assets/images/hero.png";
+
 const Hero = () => {
   return (
-    <div className="bg-hero max-sm:py-16 max-sm:px-6 sm:pt-32 sm:ps-20 md:rounded-b-[64px]">
-      Hero
+    <div className="flex md:justify-between max-md:flex-col items-start gap-10 bg-hero max-md:py-16 max-md:px-6 md:pt-32 md:ps-20 md:rounded-bl-[64px] max-md:rounded-b-[32px]">
+      <div className="dd">
+        <h1 className="md:text-[56px] md:leading-[64px] max-sm:text-[40px] max-md:leading-[48px] font-semibold text-black/85 tracking-[-0.03em]">
+          HireGO is your <span className="block">all-in-one</span> Global People
+          Platform
+        </h1>
+      </div>
+      <div className="object-contain">
+        <img
+          src={heroImage}
+          alt="Hero"
+          className="w-full h-full max-2xl:max-w-[713px] max-2xl:max-h-[536px] max-md:rounded-br-[32px]"
+        />
+      </div>
     </div>
   );
 };
